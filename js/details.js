1629b52eda3ee47a3c0ccadec6c773217ea91eaf
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-07-28 19:47:47 +0100

    Update details.js

--- js/details.js
+++ js/details.js
@@ -39,9 +39,7 @@
                     <div class="mt-4">
                         <a href="${watchUrl}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Watch Now</a>
                     </div>
-                     <div class="mt-4">
-                        <a href="${watchUrl}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Recommendations</a>
-                    </div>
+                    
                 </div>
             `;
         }
